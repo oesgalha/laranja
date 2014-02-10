6673075bf2880f5093551597f2ea07c95c9058b3
Oscar Esgalha <oscaresgalha@gmail.com>
2014-02-10 15:27:19 -0200

    Address gen now generating: street name, city name, district name, secondary address and building number.

--- lib/laranja/address.rb
+++ lib/laranja/address.rb
@@ -1,6 +1,49 @@
 module Laranja
   class Address < Base
 
+    def self.city
+      strf [
+        '%city_prefix :name%last_name',
+        '%city_prefix :name%last_name %city_suffix',
+        '%city_prefix %female_city_prefix :name%female_first_name',
+        '%city_prefix %male_city_prefix :name%male_first_name',
+        '%city_prefix %female_city_prefix :name%female_first_name %city_suffix',
+        '%city_prefix %male_city_prefix :name%male_first_name %city_suffix',
+        ':name%last_name %city_suffix',
+        '%female_city_prefix :name%female_first_name',
+        '%male_city_prefix :name%male_first_name',
+        '%female_city_prefix :name%female_first_name %city_suffix',
+        '%male_city_prefix :name%male_first_name %city_suffix'
+      ].sample
+    end
+
+    def self.district
+      strf [
+        '%district_prefix :name%last_name',
+        '%district_prefix :name%male_title :name%last_name',
+        '%district_prefix :name%female_title :name%last_name'
+      ].sample
+    end
+
+    def self.street
+      strf [
+        '%log_prefix :name%female_title :name%female_first_name :name%last_name',
+        '%log_prefix :name%female_first_name :name%last_name',
+        '%log_prefix :name%male_title :name%male_first_name :name%last_name :name%male_suffix',
+        '%log_prefix :name%male_title :name%male_first_name :name%last_name',
+        '%log_prefix :name%male_first_name :name%last_name :name%male_suffix',
+        '%log_prefix :name%male_first_name :name%last_name'
+      ].sample
+    end
+
+    def self.secondary
+      strf('%secondary ') + (1 + rand(999)).to_s
+    end
+
+    def self.number
+      (1 + rand(999)).to_s
+    end
+
     def self.formatted_cep(ufab = nil)
       cep_builder(true, rand(6) != 0, ufab)
     end
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -1,30 +1,31 @@
 require 'spec_helper'
 
-describe Laranja::Address do
-  describe '#uf' do
-    it 'returns a string' do
-      expect(Laranja::Address.uf).to be_a_kind_of(String)
+shared_examples 'non empty string generator' do |str|
+  describe 'and' do
+    it 'return a string' do
+      expect(str).to be_a_kind_of(String)
     end
 
     it 'returns a non empty string' do
-      expect(Laranja::Address.uf).not_to be_empty
+      expect(str).not_to be_empty
     end
+  end
+end
+
+describe Laranja::Address do
+  describe '#uf' do
+    it_behaves_like 'non empty string generator', Laranja::Address.uf
   end
 
   describe '#uf_abbr' do
-    it 'returns a string' do
-      expect(Laranja::Address.uf_abbr).to be_a_kind_of(String)
-    end
-
+    it_behaves_like 'non empty string generator', Laranja::Address.uf_abbr
     it 'returns a 2 characters string' do
       expect(Laranja::Address.uf_abbr.size).to eq(2)
     end
   end
 
   describe '#formatted_cep' do
-    it 'returns a string' do
-      expect(Laranja::Address.formatted_cep).to be_a_kind_of(String)
-    end
+    it_behaves_like 'non empty string generator', Laranja::Address.formatted_cep
 
     it 'returns an 8 characters string' do
       expect(Laranja::Address.formatted_cep.size).to eq(9)
@@ -44,9 +45,7 @@ describe Laranja::Address do
   end
 
   describe '#cep' do
-    it 'returns a string' do
-      expect(Laranja::Address.cep).to be_a_kind_of(String)
-    end
+    it_behaves_like 'non empty string generator', Laranja::Address.cep
 
     it 'returns an 8 characters string' do
       expect(Laranja::Address.cep.size).to eq(8)
@@ -62,6 +61,29 @@ describe Laranja::Address do
       expect(Laranja::Address.cep('rj')[0].to_i).to eq(2)
       expect(Laranja::Address.cep('RS')[0].to_i).to eq(9)
       expect(Laranja::Address.cep(:sp)[0].to_i).to eq(1)
+    end
+  end
+
+  describe '#city' do
+    it_behaves_like 'non empty string generator', Laranja::Address.city
+  end
+
+  describe '#street' do
+    it_behaves_like 'non empty string generator', Laranja::Address.street
+  end
+
+  describe '#district' do
+    it_behaves_like 'non empty string generator', Laranja::Address.district
+  end
+
+  describe '#secondary' do
+    it_behaves_like 'non empty string generator', Laranja::Address.secondary
+  end
+
+  describe '#number' do
+    it_behaves_like 'non empty string generator', Laranja::Address.number
+    it 'returns a number only string' do
+      expect(Laranja::Address.number).to match(/\d+/)
     end
   end
 end
