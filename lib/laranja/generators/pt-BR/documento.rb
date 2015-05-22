5d18fe9dce90c066af71e010d810c77023209940
oesgalha <oscaresgalha@gmail.com>
2015-05-22 20:28:12 -0300

    Clearer now, I guess...

--- lib/laranja/generators/pt-BR/documento.rb
+++ lib/laranja/generators/pt-BR/documento.rb
@@ -25,15 +25,22 @@ module Laranja
         data('codigo_uf')[uf.to_s.downcase].to_s
       end
 
-      def verification_digits(cpf)
-        v1, v2 = 0, 0
+      def v1_digit(cpf)
         v1 = (0..8).reduce(0) { |sum, i| sum + cpf[i].to_i * (10 - i) }
-        v2 = (0..8).reduce(0) { |sum, i| sum + cpf[i].to_i * (11 - i) }
         v1 %= 11
-        v1 = v1 < 2 ? 0 : 11 - v1
+        v1 < 2 ? 0 : 11 - v1
+      end
+
+      def v2_digit(cpf, v1)
+        v2 = (0..8).reduce(0) { |sum, i| sum + cpf[i].to_i * (11 - i) }
         v2 += v1 * 2
         v2 %= 11
-        v2 = v2 < 2 ? 0 : 11 - v2
+        v2 < 2 ? 0 : 11 - v2
+      end
+
+      def verification_digits(cpf)
+        v1 = v1_digit(cpf)
+        v2 = v2_digit(cpf, v1)
         [v1, v2]
       end
     end
